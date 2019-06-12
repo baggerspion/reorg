12a59f0cbe376447afe4d04b0997c44c48156775
Paul Adams <paul@baggerspion.net>
2019-06-12 10:01:41 +0200

    Added support for reviews

--- src/schema.rs
+++ src/schema.rs
@@ -22,6 +22,16 @@ table! {
 }
 
 table! {
+    reviews (id) {
+        id -> Int4,
+        reviewer_id -> Int4,
+        submission_id -> Int4,
+        private_comments -> Varchar,
+        shared_comments -> Varchar,
+    }
+}
+
+table! {
     submissions (id) {
         id -> Int4,
         conference_id -> Int4,
@@ -44,6 +54,7 @@ table! {
 allow_tables_to_appear_in_same_query!(
     conferences,
     reviewers,
+    reviews,
     submissions,
     users,
 );

3f451929c9d8bc418b0610bb21e962d4bf31742c
Paul Adams <paul@baggerspion.net>
2019-06-24 16:17:03 +0200

    Receive all reviews (shared/private) by submission id

--- src/queries/mod.rs
+++ src/queries/mod.rs
@@ -1,4 +1,6 @@
 use crate::data::DbConnection;
+use crate::review::model::Review;
+use crate::review::schema::reviews::dsl::{reviews, submission_id};
 use crate::submission::model::Submission;
 use crate::submission::schema::submissions::dsl::{submissions, conference_id};
 use diesel;
@@ -17,6 +19,16 @@ fn read_conf_subs(cid: i32, conn: DbConnection) -> Result<JsonValue, Status> {
     submissions
         .filter(conference_id.eq(cid))
         .load::<Submission>(&*conn)
+        .map(|item| json!(item))
+        .map_err(|_| Status::NotFound)
+}
+
+/* Get the reviews to a given submission */
+#[get("/submission/<sid>/reviews")]
+fn read_sub_revs(sid: i32, conn: DbConnection) -> Result<JsonValue, Status> {
+    reviews
+        .filter(submission_id.eq(sid))
+        .load::<Review>(&*conn)
         .map(|item| json!(item))
         .map_err(|_| Status::NotFound)
 }

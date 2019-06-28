ebb00b895f112002e16028c3250caf53c7706704
Paul Adams <paul@baggerspion.net>
2019-06-28 04:25:44 +0200

    Added status_id reference to submissions

--- src/submission/schema.rs
+++ src/submission/schema.rs
@@ -3,6 +3,7 @@ table! {
         id -> Nullable<Int4>,
         conference_id -> Int4,
         user_id -> Int4,
+        status_id -> Int4,
         title -> Varchar,
         content -> Varchar,
     }

b1407e55219c525066c605468a537362d9b5b047
Paul Adams <paul@baggerspion.net>
2020-03-28 19:22:10 +0100

    Add link to about page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
+
 export default function Index() {
   return (
     <div>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
       <h1>Hello World!</h1>
     </div>
   );

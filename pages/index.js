8323fec507d14bccbfde93c121f8e125f041f532
Paul Adams <paul@baggerspion.net>
2020-03-29 21:53:44 +0200

    Display conference names

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import Layout from '../components/Layout';
+import fetch from '../components/Fetch'
+import Layout from '../components/Layout'
+import useSWR from 'swr'
 
 export default function Index() {
+  const { data, error } = useSWR('/api/conferences', fetch);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  
+  if (error) return <div>failed to load</div>
+
   return (
     <Layout>
-      <h1>Hello World!</h1>
+      <div>
+        <ul>
+        {
+        data ? data.conferences.map(conference =>
+	  <li>{conference.data.title}: {conference.data.start_date}</li>
+	) : <p>Loading...</p>
+        }
+	</ul>
+      </div>
     </Layout>
   );
 }

4efaee43ed234d6933d04f5050d2c38c4fabf62f
Paul Adams <paul@baggerspion.net>
2020-03-30 15:42:03 +0200

    Add support for listing reviewers

--- pages/conference/[id].js
+++ pages/conference/[id].js
@@ -3,6 +3,15 @@ import Layout from '../../components/Layout';
 import { useRouter } from 'next/router';
 import useSWR from 'swr'
 
+const Reviewer = props => {
+    const { data, error } = useSWR(`/api/user?id=${props.id}`, fetch);
+    if (error) return <div>Failed to load conference data!</div>
+
+    return (
+        <li>{data ? data.data.first_name + " " + data.data.last_name : "Loading..."}</li>
+    );
+};
+
 export default function Conference() {
     const router = useRouter();
     const { data, error } = useSWR(`/api/conference?conf=${router.query.id}`, fetch);
@@ -16,6 +25,13 @@ export default function Conference() {
             <h3>Applicants</h3>
             <h3>Resources</h3>
             <h3>Reviewers</h3>
+            <ul>
+            {
+            data.data.reviewers ? (
+                data.data.reviewers.map(rev => <Reviewer id={rev} />
+            )) : ( <li>This conference has no reviewers.</li> )
+            }
+            </ul>
         </Layout>
     );
 }

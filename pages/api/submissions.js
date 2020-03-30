aba99ef0fddf4995a257903bdf2f1cf2032147c3
Paul Adams <paul@baggerspion.net>
2020-03-30 22:34:38 +0200

    Show submissions count

---
+++ pages/api/submissions.js
@@ -0,0 +1,33 @@
+import faunadb, { query as q } from 'faunadb';
+
+const { FAUNADB_SECRET: secret } = process.env;
+
+let client;
+
+if (secret) {
+  client = new faunadb.Client({ secret });
+}
+
+export default async (req, res) => {
+  try {
+    if (!client) {
+      return [];
+    }
+
+    let submissions = [];
+
+    await client.paginate(
+      q.Match(
+        q.Index('subs_conference'), req.query.id
+      )
+    )    
+    .map(ref => q.Get(ref))
+    .each(page => {
+      submissions = submissions.concat(page);
+    });   
+	  
+    res.json(submissions);
+  } catch (error) {
+    res.status(500).json({ error });
+  }     
+}
--- pages/conference/[id].js
+++ pages/conference/[id].js
@@ -3,6 +3,16 @@ import Layout from '../../components/Layout';
 import { useRouter } from 'next/router';
 import useSWR from 'swr'
 
+const Applicants = props => {
+    const { data, error } = useSWR(`/api/submissions?id=${props.id}`, fetch);
+    if (error) return <div>Failed to load submissions data!</div>
+    if (!data) return <div>Loading...</div>
+
+    return (
+        <p>{data.length > 0 ? <b>{data.length}</b> : "No"} {data.length == 1 ? "submission" : "submissions"} received.</p>
+    );
+};
+
 const Reviewer = props => {
     const { data, error } = useSWR(`/api/user?id=${props.id}`, fetch);
     if (error) return <div>Failed to load conference data!</div>
@@ -23,6 +33,7 @@ export default function Conference() {
         <Layout>
             <h2>{data.data.title}: {data.data.start_date}</h2>
             <h3>Applicants</h3>
+            <Applicants id={data['ref']['@ref']['id']} />
             <h3>Resources</h3>
             <h3>Reviewers</h3>
             <ul>

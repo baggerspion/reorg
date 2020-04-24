eaea522fb3b4d757634d39b552375fb087d4bc39
Paul Adams <paul@baggerspion.net>
2020-04-24 12:00:01 +0200

    Cleanup

--- libs/apolloClient.js
+++ libs/apolloClient.js
@@ -6,12 +6,10 @@ import fetch from 'isomorphic-unfetch';
 const secret = process.env.FAUNADB_SECRET;
 
 export default function createApolloClient(initialState, ctx) {
-    // The `ctx` (NextPageContext) will only be present on the server.
-    // use it to extract auth headers (ctx.req) or similar.
     return new ApolloClient({
         ssrMode: Boolean(ctx),
         link: new HttpLink({
-            uri: 'https://graphql.fauna.com/graphql', // Server URL (must be absolute)
+            uri: 'https://graphql.fauna.com/graphql',
             headers: {
                 authorization: `Bearer ${secret}`
             },

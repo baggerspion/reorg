3fccf1c73d545a72499f7ba8bd0ff67783c4c613
Paul Adams <paul@baggerspion.net>
2020-04-25 18:49:24 +0200

    Improved query for fewer requests. You can thank me later, Fauna

--- gql/allConferences.js
+++ gql/allConferences.js
@@ -2,7 +2,20 @@ import gql from 'graphql-tag';
 
 const allConferences = () => {
     return (
-        gql`query allConferences { allConferences { data { _id, name, short_name } } }`
+        gql`query allConferences { 
+            allConferences { 
+                data { 
+                    _id, 
+                    name, 
+                    short_name
+                    submissions {
+                        data {
+                            status
+                        }
+                    }
+                } 
+            } 
+        }`
     );
 }
 
